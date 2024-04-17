68da7ac78cc59d59a2a916de613a720239e2594d
serm-webFox <109290607+serm-webFox@users.noreply.github.com>
2024-04-17 19:23:29 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<script>
+  export default{
+    data(){
+      return{
+        cases: [],
+        case: ''
+      }
+    },
+    methods:{
+      //Метод для добавления элемента case в массив cases
+      addCase(){
+        //Проверка на пустоту
+        if (this.case != '') this.cases.push( this.case )
+        //Очистка case
+        this.case = ''
+      },
+      //Метод для удаления элемента case из массива cases
+      delCase(i){
+        this.cases.splice(i, 1)
+      }
+    }
+  }
+</script>
+
+<template>
+  <div>Список дел</div>
+  <input type="text" v-model="this.case">
+  <button @click="addCase">Добавить</button>
+  <hr>
+
+  <ul>
+    <li v-for="(item, i) in cases">
+      <div>{{ item }}</div>
+      <button @click="delCase(i)">Удалить</button>
+    </li>
+  </ul>
+
+  <hr>
+  <div>Всего дел {{ cases.length }} </div>
+</template>
+
+<style>
+  ul{
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+  li{
+    width: 245px;
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 0;
+  }
+</style>
